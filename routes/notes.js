b05c42ef19f0312a137dbaa54cc52a1849d253eb
Haley Myers <haleynmyers@gmail.com>
2020-09-18 16:18:15 -0500

    More server code

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,15 @@
-var app = require("express").Router();
+var app = require("express").Router(); 
 var path = require("path");
 var db = require("../db/db.json");
 var fs = require("fs");
 
+
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    db = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+    db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(db);
 });
 
@@ -28,21 +29,21 @@ app.post("/api/notes", function(req, res) {
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-    var remainingNotes = [];
+    var undeletedNotes = [];
     for (var i = 0; i < db.length; i++) {
         if (db[i].id != req.params.id) {
-            remainingNotes.push(db[i]);
+            undeletedNotes.push(db[i]);
         }
     }
-    db = remainingNotes;
+    db = undeletedNotes;
     fs.writeFileSync("./db/db.json", JSON.stringify(db), function(err, res) {
         if (err) {
             throw err;
         }
     });
+    console.log("Delete", db);
     res.json(db);
 });
-
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+var routes = require("./routes");
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public"));run
 app.use(routes);
 
 app.listen(PORT, function() {
